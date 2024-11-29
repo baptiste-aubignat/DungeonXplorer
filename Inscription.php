c4b72af6414093c495c95857d94b15823e78a65e
LouMek <lmekhelef@gmail.com>
2024-11-29 15:38:03 +0100

    Modification page Inscription

--- Inscription.php
+++ Inscription.php
@@ -1,3 +1,36 @@
+<?php
+session_start();
+
+
+include_once "param_connexion.php";
+
+$user = $db_username;
+$pass = $db_password;
+$conn = new PDO("mysql:host=dungeondb-dungeon.j.aivencloud.com;dbname=dungeon;port=11901", $user, $pass);
+
+if (isset($_POST['envoi'])) {
+    if (
+        !empty($_POST['pseudo']) and !empty($_POST['password'])
+        and !empty($_POST['email']) and !empty($_POST['passwordvalid'])
+    ) {
+        if ($_POST['passwordvalid'] != $_POST['password']) {
+            echo "mdp différent";
+            return false;
+        }
+
+        echo "Compte crée";
+        $pseudo = htmlspecialchars($_POST['pseudo']);
+        $password = sha1($_POST['password']);
+        $email = htmlspecialchars($_POST['email']);
+
+
+        $insertUser = $conn->prepare('INSERT INTO Account(name, email, password) VALUES(?, ?, ?)');
+        $insertUser->execute(array($pseudo, $email, $password));
+    } else {
+        echo "Merci de completer tous les champs";
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -25,7 +58,7 @@
         <div class="container" style="max-width: 700px;">
             <div class="column">
 
-                <form class="box boxConnexion">
+                <form method="POST" action="" class="box boxConnexion">
 
                     <h1 class="titreConnexion"> Inscription </h1>
 
@@ -38,14 +71,14 @@
                                 <i class="fas fa-user"></i>
                             </span>
 
-                            <!--Si pseudo non valide lors de la validation-->
+                            <!--Si pseudo non valide lors de la validation
                             <div id="pseudoOuMailNonValide">
-                                <input class="input is-danger" type="text" placeholder="Pseudo">
+                                <input name="Pseudo" class="input is-danger" type="text" placeholder="Pseudo">
                                 <p class="help is-danger">Pseudo déjà utilisé</p>
-                            </div>
+                            </div> -->
                             <!--Sinon-->
                             <div id="pseudoOuMailValide">
-                                <input class="input" type="text" placeholder="Pseudo">
+                                <input name="pseudo" class="input" type="text" placeholder="Pseudo" autocomplete="off">
                             </div>
 
                         </div>
@@ -60,14 +93,14 @@
                                 <i class="fa-solid fa-envelope"></i>
                             </span>
 
-                            <!--Si mail non valide lors de la validation-->
+                            <!--Si mail non valide lors de la validation
                             <div id="pseudoOuMailNonValide">
-                                <input class="input is-danger" type="email" placeholder="Exemple@Dungeon.xplorer">
+                                <input name="email" class="input is-danger" type="email" placeholder="Exemple@Dungeon.xplorer">
                                 <p class="help is-danger">Mail non valide</p>
-                            </div>
+                            </div> -->
                             <!--Sinon-->
                             <div id="pseudoOuMailValide">
-                                <input class="input" type="email" placeholder="Exemple@Dungeon.xplorer">
+                                <input name="email" class="input" type="email" placeholder="Exemple@Dungeon.xplorer" autocomplete="off">
                             </div>
 
                         </div>
@@ -82,28 +115,25 @@
                             <span class="icon is-small is-left">
                                 <i class="fa-solid fa-lock"></i>
                             </span>
-                            
+
                             <div>
-                                <input class="input" type="password" placeholder="Mot de passe">
-                            </div> 
+                                <input name="password" class="input" type="password" placeholder="Mot de passe" autocomplete="off">
+                            </div>
                         </div>
                     </div>
 
-                    <!--Mot de passe x2-->
+                    <!-- Mot de passe x2 -->
                     <div class="field">
                         <div class="control has-icons-left">
-                            <!--Icone-->
                             <span class="icon is-small is-left">
                                 <i class="fa-solid fa-lock"></i>
                             </span>
-                            <!--Si Mot de passe différent lors de la validation-->
-                            <div id="mdpNonValide">
-                                <input class="input is-danger" type="password" placeholder="Récrivez votre mot de passe">
+                            <!-- <div id="mdpNonValide">
+                                <input name="passwordValid" class="input is-danger" type="password" placeholder="Récrivez votre mot de passe">
                                 <p class="help is-danger">Mot de passe différent !</p>
-                            </div>
-                            <!--Sinon-->
+                            </div> -->
                             <div id="mdpValide">
-                                <input class="input" type="password" placeholder="Récrivez votre mot de passe">
+                                <input name="passwordvalid" class="input" type="password" placeholder="Récrivez votre mot de passe">
                             </div>
                         </div>
                     </div>
@@ -111,7 +141,7 @@
 
                     <div class="field is-grouped">
                         <div class="control">
-                            <button class="button is-light">Créer</button>
+                            <button name="envoi" class="button is-light">Créer</button>
                         </div>
                         <div class="control">
                             <button class="button boutonOr">Annuler</button>
