3425c58e2f85c9a29d8506e61ac6acfd6d560f4c
aubignat <baptiste.aubignat@etu.unicaen.fr>
2024-11-29 14:08:22 +0100

    petites corrections du header et du footer

--- index.php
+++ index.php
@@ -5,10 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>DungeonXplorer</title>
         <link rel="icon" href="images/Logo.png" type="image/png">
-
         <link rel="stylesheet" href="styles/bulma.min.css">
         <link rel="stylesheet" href="styles/style.css">
-
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Pirata+One&display=swap" rel="stylesheet">
@@ -18,12 +16,11 @@
         <header>
             <nav class="navbar" aria-label="main navigation">
                 <div class="navbar-brand pl-6">
-                    <a class="navbar-item" href="#">
+                    <a class="navbar-item" href="index.php">
                         <figure class="image">
                             <img src="images/Logo.png" alt="logo DungeonXplorer" class="is-rounded logo aligneBas">
                         </figure>
                     </a>
-
                     <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
                         <span aria-hidden="true"></span>
                         <span aria-hidden="true"></span>
@@ -31,42 +28,36 @@
                         <span aria-hidden="true"></span>
                     </a>
                 </div>
-
                 <div class="navbar-menu">
                     <div class="navbar-start">
-                        <a class="navbar-item tprincipal" href="#">
+                        <a class="navbar-item tprincipal" href="index.php">
                             Home
                         </a>
                     </div>
-
                     <div class="navbar-end pr-6">
                         <div class="navbar-item">
                             <div class="buttons">
-                                <a class="button boutonOr">
+                                <a class="button boutonOr" href="inscription.php">
                                 S'inscrire
                                 </a>
-                                <a class="button is-light">
+                                <a class="button is-light" href="connexion.php">
                                 Se connecter
                                 </a>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </nav>
         </header>
-
         <main>
             <div class="container pt-large px-5">
                 <div class="notification bsecondaire has-text-centered">
                     <figure class="image pb-3">
                         <img src="images/Logo.png" alt="logo DungeonXplorer" class="is-rounded grand-logo aligneHaut center">
                     </figure>
-                    
                     <h1 class="tprincipal pb-1 is-size-3 is-size-4-mobile has-text-weight-bold">
                         Bienvenue sur DungeonXplorer
                     </h1>
-
                     <p class="tsecondaire is-size-5-desktop is-size-6-tablet is-size-7-mobile px-4 has-text-justified">
                         Votre portail vers un monde de dark fantasy, où chaque choix vous rapproche un peu plus des ténèbres... ou de la lumière. 
                         Plongez dans un univers impitoyable inspiré des classiques du "livre dont vous êtes le héros" : ici, chaque décision compte et peut être votre dernière. 
@@ -85,7 +76,7 @@
               </div>
         </main>
         <footer>
-            <p>&copy; Pixels & Parchelins 2024</p>
+            <p>&copy; Pixels & Parchemins 2024</p>
         </footer>
         <script defer src="scripts/script.js"></script>
     </body>
