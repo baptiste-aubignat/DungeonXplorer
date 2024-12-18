9f75498b4002aeff1ed33e4a7ba589d2598ee482
aubignat <baptiste.aubignat@etu.unicaen.fr>
2024-12-18 11:44:45 +0100

    ajout de la police Roboto et du favicon sur la page de connexion

--- connexionUtilisateur.php
+++ connexionUtilisateur.php
@@ -43,6 +43,7 @@ if (isset($_POST['envoiConnexion'])) {
     <!--Lignes en dessous à modifer en fonction de la page-->
     <meta name="description" content="Page de connexion à DongeonXplorer">
     <title> Connexion </title>
+    <link rel="icon" href="images/Logo.png" type="image/png">
 
     <link rel="stylesheet" href="styles/style.css">
     <!-- Bulma -->
@@ -54,6 +55,7 @@ if (isset($_POST['envoiConnexion'])) {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Pirata+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 </head>
 
 <body>
