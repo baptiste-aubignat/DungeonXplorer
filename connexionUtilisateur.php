de179e3f71e76665463d21990b898586053aa3ed
aubignat <baptiste.aubignat@etu.unicaen.fr>
2024-12-18 11:48:54 +0100

    ajout du header et du footer sur la page de connexion

--- connexionUtilisateur.php
+++ connexionUtilisateur.php
@@ -59,6 +59,42 @@ if (isset($_POST['envoiConnexion'])) {
 </head>
 
 <body>
+    <header>
+        <nav class="navbar" aria-label="main navigation">
+            <div class="navbar-brand pl-6">
+                <a class="navbar-item" href="index.php">
+                    <figure class="image">
+                        <img src="images/Logo.png" alt="logo DungeonXplorer" class="is-rounded logo aligneBas">
+                    </figure>
+                </a>
+                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+            <div class="navbar-menu">
+                <div class="navbar-start">
+                    <a class="navbar-item tprincipal" href="index.php">
+                        Home
+                    </a>
+                </div>
+                <div class="navbar-end pr-6">
+                    <div class="navbar-item">
+                        <div class="buttons">
+                            <a class="button boutonOr" href="inscriptionUtilisateur.php">
+                                S'inscrire
+                            </a>
+                            <a class="button is-light" href="connexionUtilisateur.php">
+                                Se connecter
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
     <div class="section">
         <div class="container" style="max-width: 700px;">
             <div class="column">
@@ -125,6 +161,10 @@ if (isset($_POST['envoiConnexion'])) {
             </div>
         </div>
     </div>
+    <footer>
+        <p>&copy; Pixels & Parchemins 2024</p>
+    </footer>
+    <script defer src="scripts/script.js"></script>
 </body>
 
 </html>
